66a1bb96944584c0c878ba4b1f787c8c38668e94
Serge Kosachiov <skosachiov@gmail.com>
2026-04-23 16:03:06 +0300

    ok

--- crx2deb/build.sh
+++ crx2deb/build.sh
@@ -2,6 +2,8 @@
 set -e
 
 EXTENSION_PREFIX="chromium-extension"
+MAINTAINER_NAME="Your Name"
+MAINTAINER_EMAIL="your.email@example.com"
 
 # Locate and decode CRX
 SRC_FILE=$(find src -maxdepth 1 -type f ! -name ".*" | head -1)
@@ -15,11 +17,22 @@ CRX_FILE=$(basename "$SRC_FILE" .base64)
 base64 -d "$SRC_FILE" > tmp/$CRX_FILE
 
 # Extract metadata
-CRX_NAME=$(python3 debian/scripts/crx_reader.py --name tmp/$CRX_FILE)
+CRX_NAME=$(python3 debian/scripts/crx_reader.py --name --sanitize tmp/$CRX_FILE)
 CRX_ID=$(python3 debian/scripts/crx_reader.py --id tmp/$CRX_FILE)
 CRX_VERSION=$(python3 debian/scripts/crx_reader.py --version tmp/$CRX_FILE)
 
+mv tmp/$CRX_FILE tmp/$CRX_NAME.crx
+
 echo "Extension: $CRX_NAME ($CRX_ID) version $CRX_VERSION"
+
+cat > debian/vars.mk <<EOF
+EXTENSION_PREFIX = $EXTENSION_PREFIX
+CRX_NAME = $CRX_NAME
+CRX_ID = $CRX_ID
+CRX_VERSION = $CRX_VERSION
+MAINTAINER_NAME = ${MAINTAINER_NAME:-Your Name}
+MAINTAINER_EMAIL = ${MAINTAINER_EMAIL:-your.email@example.com}
+EOF
 
 # Generate debian/changelog from template
 sed -e "s/@PACKAGE@/$CRX_NAME/g" \
@@ -33,17 +46,11 @@ sed -e "s/@PACKAGE@/$CRX_NAME/g" \
     debian/changelog.in > debian/changelog
 
 # Generate debian/control from control.in
-sed -e "s/@CRX_ID@/$CRX_ID/g" \
-    -e "s/@CRX_NAME@/$CRX_NAME/g" \
+sed -e "s/@CRX_NAME@/$CRX_NAME/g" \
     -e "s/@CRX_VERSION@/$CRX_VERSION/g" \
     -e "s/@EXTENSION_PREFIX@/$EXTENSION_PREFIX/g" \
     -e "s/@MAINTAINER_NAME@/${MAINTAINER_NAME:-Your Name}/g" \
     -e "s/@MAINTAINER_EMAIL@/${MAINTAINER_EMAIL:-your.email@example.com}/g" \
     debian/control.in > debian/control
 
-# Optionally generate debian/copyright from copyright.in
-# (Add appropriate sed substitutions for copyright years, author, license)
-# sed ... debian/copyright.in > debian/copyright
 
-# Now build the package
-dpkg-buildpackage -us -uc -b
